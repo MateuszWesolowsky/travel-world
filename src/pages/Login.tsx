e3b0f9eca793217ae827b8067014c6ca4c7a84ba
Mateusz <m.wesolowski.92@gmail.com>
2023-10-16 23:04:22 +0200

    add class for submit button

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,7 +3,7 @@ import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
 
 export default function Login() {
-	// PRE-FILL FOR DEV PURPOSES
+
 	const [email, setEmail] = useState("jack@example.com");
 	const [password, setPassword] = useState("qwerty");
 
@@ -32,7 +32,7 @@ export default function Login() {
 				</div>
 
 				<div>
-					<button>Login</button>
+					<button className={styles.ctaLink}>Login</button>
 				</div>
 			</form>
 		</main>
