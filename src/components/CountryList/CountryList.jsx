af42ed6293c5688dd08b373250015b2b58c91fa6
Mateusz <m.wesolowski.92@gmail.com>
2023-11-04 23:19:19 +0100

    create CountryList component

---
+++ src/components/CountryList/CountryList.jsx
@@ -0,0 +1,33 @@
+import { useCities } from "../../context/CitiesContex";
+import CountryItem from "../CountryItem/CountryItem";
+import styles from "./CountryList.module.css";
+import Message from "../Message/Message";
+import Spinner from "../Spinner/Spinner";
+
+const CountryList = () => {
+	const { cities, isLoading } = useCities();
+	if (isLoading) return <Spinner />;
+
+	if (!cities.length)
+		return (
+			<Message message='Add Your first country by clicking on a country on the map' />
+		);
+
+	const countries = cities.reduce((arr, city) => {
+		if (!arr.map((el) => el.country).includes(city.country)) {
+			return [...arr, city];
+		} else {
+			return arr;
+		}
+	}, []);
+
+	return (
+		<ul className={styles.countryList}>
+			{countries.map((country) => (
+				<CountryItem country={country} key={country.id} />
+			))}
+		</ul>
+	);
+};
+
+export default CountryList;
