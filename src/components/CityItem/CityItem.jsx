e26d144280b21e176cffc396f748bccb88a9d145
Mateusz <m.wesolowski.92@gmail.com>
2023-11-08 23:44:16 +0100

    add active class for current City element

--- src/components/CityItem/CityItem.jsx
+++ src/components/CityItem/CityItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
+import { useCities } from "../../context/CitiesContex";
 
 
 const formatDate = (date) =>
@@ -10,10 +11,10 @@ const formatDate = (date) =>
 	}).format(new Date(date));
 
 function CityItem({ city }) {
-	
+	const {currentCity} = useCities();
 	return (
 		<li >
-			<Link className={styles.cityItem} to={`${city.id}?lat=${city.position.lat}&lng=${city.position.lng}`}>
+			<Link className={`${styles.cityItem} ${city.id === currentCity.id ? styles['cityItem--active'] : ""}`} to={`${city.id}?lat=${city.position.lat}&lng=${city.position.lng}`}>
 			<span className={styles.emoji}>{city.emoji}</span>
 			<h3 className={styles.name}>{city.cityName}</h3>
 			<time className={styles.date}>({formatDate(city.date)})</time>
