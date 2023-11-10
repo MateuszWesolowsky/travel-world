66946325ca64dba584e5f41f89de91aa88996026
Mateusz <m.wesolowski.92@gmail.com>
2023-11-10 15:44:16 +0100

    add createCity func and deleteCity func

--- src/context/CitiesContex.jsx
+++ src/context/CitiesContex.jsx
@@ -32,7 +32,38 @@ function CitiesProvider({ children }) {
 			const data = await res.json();
 			setCurrentCity(data);
 		} catch {
+			alert(`There was an error creating city.`);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+	async function createCity(newCity) {
+		try {
+			setIsLoading(true);
+			const res = await fetch(`${BASE_URL}/cities`, {
+				method: "POST",
+				body: JSON.stringify(newCity),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			const data = await res.json();
+			setCities((cities) => [...cities, data]);
+		} catch {
 			alert(`There was an error loading..`);
+		} finally {
+			setIsLoading(false);
+		}
+	}
+	async function deleteCity(id) {
+		try {
+			setIsLoading(true);
+			await fetch(`${BASE_URL}/cities/${id}`, {
+				method: "DELETE",
+			});
+			setCities((cities) => cities.filter((city) => city.id !== id));
+		} catch {
+			alert(`There was an deleteing city.`);
 		} finally {
 			setIsLoading(false);
 		}
@@ -45,6 +76,8 @@ function CitiesProvider({ children }) {
 				isLoading,
 				currentCity,
 				getCity,
+				createCity,
+				deleteCity,
 			}}>
 			{children}
 		</CitiesContext.Provider>
